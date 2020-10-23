1059fdec34c1506a37fe6b85aad10b790fa2d124
DariusRain <dariusrain.student@careerdevs.com>
2020-10-22 21:26:00 -0400

    Cleaning up more code

--- modules/compiler.js
+++ modules/compiler.js
@@ -1,24 +1,43 @@
 const fs = require("fs");
-let allBizs = [];
-let fileNames = fs.readdirSync("./data/scraped/");
 
-module.exports = () => {
-    // let count = 0;
-    // For each file name in the "scraped" directory
-    fileNames.forEach(fileName => {
-        // count++;
-        // Get file from scraped directory with current fileName in itteration alphabetically.
-        let file = fs.readFileSync(`./scraped/${fileName}`);
+//  Compiles x amount of JSON files from a directory path into one JSON file which will only contain an arrray.
+class Compiler {
 
-        // Add current bizs array ineach json file to the allBizs array which will later be added to a large json file.
-        allBizs.push(...JSON.parse(file).bizs);
-    })
+    // Container for all collected elements until parsed into one JSON file.
+    allBizs = []
+    
+    // Path is the directory path that has all the JSON files.
+    constructor(path) {
+        this.path = path;
+    }
+    
+    compile() {
+
+        // Get array of file names in directory
+        let fileNames = fs.readdirSync(path);
+
+        // For each file name in the "scraped" directory
+        fileNames.forEach(fileName => {
+
+            // Get file from scraped directory with current fileName in itteration alphabetically.
+            let file = fs.readFileSync(`${path}${fileName}`);
+
+            // Add current bizs array ineach json file to the allBizs array which will later be added to a large json file.
+            this.allBizs.push(...JSON.parse(file).bizs);
+        })
 
 
-    //  Create json file of all bizs pushed to the array
-    fs.writeFileSync("allBizsRaw.json", JSON.stringify(allBizs));
+        //  Create json file of all bizs pushed to the array
+        fs.writeFileSync("allBizsRaw.json", JSON.stringify(allBizs));
 
-    // Debug:
-    // console.log("count:", count);
-    console.log("Bizs:", allBizs.length);
-} 
+        // Debug:
+        // console.log("count:", count);
+        console.log("Compiled: ", allBizs.length);
+
+    }
+}
+
+
+
+
+module.exports = Compiler;
